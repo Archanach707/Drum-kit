4a54cb69932c909318eb171ffbf73a8c1396be3c
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-11-24 19:08:49 +0530

    apply for loop for all button click

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-document.querySelectorAll(".drum")[i].addEventListener("click",function () {
-  alert("i got Clicked!");
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-});
+for (var i = 0; i<numberOfDrumButtons; i++) {
+  
+  document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+    alert("i got Clicked!");
+  
+  });
+  
+}
