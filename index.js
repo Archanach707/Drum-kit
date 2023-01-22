68a3bed38cd017e2b57e4e9ac4c03e71aae3fa00
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2023-01-22 19:08:03 +0530

    play sounds by javascript

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
   
   document.querySelectorAll(".drum")[i].addEventListener("click",function () {
     alert("i got Clicked!");
-  
+
+    var audio = new Audio("sounds/02.mp3")
+      audio.play();
+      
   });
   
   
