6bc9b98d56671c50defbf8bd1d79dee4460cc331
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2023-01-26 22:08:29 +0530

    apply switch case for play sounds

--- index.js
+++ index.js
@@ -3,12 +3,56 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numberOfDrumButtons; i++) {
   
   document.querySelectorAll(".drum")[i].addEventListener("click",function () {
-    alert("i got Clicked!");
+    // alert("i got Clicked!");
 
-    var audio = new Audio("sounds/02.mp3")
-      audio.play();
-      
+    var buttonInnerHTML = this.innerHTML;
+    
+    switch (buttonInnerHTML) {
+      case "w":
+        var tom02 = new Audio("sounds/tom-02.mp3");
+        tom02.play();
+        break;
+
+        case "a":
+          var tom04 = new Audio("sounds/tom-04.mp3");
+          tom04.play();
+          break;
+
+        case "s":
+          var tom05 = new Audio("sounds/tom-05.mp3");
+          tom05.play();
+          break;    
+
+        case "d":
+          var tom06 = new Audio("sounds/tom-06.mp3");
+          tom06.play();
+          break;
+
+        case "j":
+          var tom02 = new Audio("sounds/tom-02.mp3");
+          tom02.play();
+          break;
+
+        case "k":
+          var tom03 = new Audio("sounds/tom-03.mp3");
+          tom03.play();
+          break;
+  
+        case "l":
+          var tom04 = new Audio("sounds/tom-04.mp3");
+          tom04.play();
+          break;
+
+      default: console.log(buttonInnerHTML)
+
+    }
+
   });
   
-  
 }
+
+document.addEventListener("keypress", function(event) {
+  
+console.log(event);
+
+});
