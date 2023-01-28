0dbfd88997d45281ecb0916775e6c3f7c1c4b1c9
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2023-01-28 18:57:16 +0530

    The drum kit project is commpleted, and the last added animation click on the button

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
     
     makeSound(buttonInnerHTML);
 
+    buttonAnimation(buttonInnerHTML);
+
   });
   
 }
@@ -17,6 +19,8 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
 document.addEventListener("keypress", function(event) {
   
   makeSound(event.key);
+
+  buttonAnimation(event.key);
 
 });
 
@@ -62,4 +66,15 @@ function makeSound(key) {
 
   }
 
+}
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
